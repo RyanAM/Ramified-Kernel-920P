d205176b260b81bdc7fa932a90fe0f36d1ddbac3
RyanAM <RyanAM@ram920054@gmail.com>
2016-03-20 17:34:12 -0700

    first commit

---
+++ Ramified-Kernel-920P/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 17 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm64"
+#define UTS_VERSION "#17 SMP PREEMPT Sat Mar 19 18:10:20 PDT 2016"
+#define LINUX_COMPILE_BY "RAM9200"
+#define LINUX_COMPILE_HOST "RAM9200"
+#define LINUX_COMPILER "gcc version 4.9 20150123 (prerelease) (GCC) "
